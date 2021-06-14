6cf8747c36bd513647618404d27b25ea7abeb23a
mo <michael.orth@src-gmbh.de>
2021-06-14 23:10:37 +0200

    refactor Mp3VoiceSplitter.py

--- python/Mp3VoiceSplitter.py
+++ python/Mp3VoiceSplitter.py
@@ -109,92 +109,18 @@ def frame_generator(frame_duration_ms, audio, sample_rate):
     offset = 0
     timestamp = 0.0
     duration = (float(n) / sample_rate) / 2.0
+
     while offset + n < len(audio):
         yield Frame(audio[offset:offset + n], offset, timestamp, duration)
         timestamp += duration
         offset += n
 
 
-def vad_collector_orig(sample_rate, frame_duration_ms,
-                       padding_duration_ms, vad, frames):
-    """Filters out non-voiced audio frames.
-    Given a webrtcvad.Vad and a source of audio frames, yields only
-    the voiced audio.
-    Uses a padded, sliding window algorithm over the audio frames.
-    When more than 90% of the frames in the window are voiced (as
-    reported by the VAD), the collector triggers and begins yielding
-    audio frames. Then the collector waits until 90% of the frames in
-    the window are unvoiced to detrigger.
-    The window is padded at the front and back to provide a small
-    amount of silence or the beginnings/endings of speech around the
-    voiced frames.
-    Arguments:
-    sample_rate - The audio sample rate, in Hz.
-    frame_duration_ms - The frame duration in milliseconds.
-    padding_duration_ms - The amount to pad the window, in milliseconds.
-    vad - An instance of webrtcvad.Vad.
-    frames - a source of audio frames (sequence or generator).
-    Returns: A generator that yields PCM audio data.
-    """
-    num_padding_frames = int(padding_duration_ms / frame_duration_ms)
-    # We use a deque for our sliding window/ring buffer.
-    ring_buffer = collections.deque(maxlen=num_padding_frames)
-    # We have two states: TRIGGERED and NOTTRIGGERED. We start in the
-    # NOTTRIGGERED state.
-    triggered = False
-
-    voiced_frames = []
-    for frame in frames:
-        is_speech = vad.is_speech(frame.bytes, sample_rate)
-
-        sys.stdout.write('1' if is_speech else '0')
-        if not triggered:
-            ring_buffer.append((frame, is_speech))
-            num_voiced = len([f for f, speech in ring_buffer if speech])
-            # If we're NOTTRIGGERED and more than 90% of the frames in
-            # the ring buffer are voiced frames, then enter the
-            # TRIGGERED state.
-            if num_voiced > 0.9 * ring_buffer.maxlen:
-                triggered = True
-                sys.stdout.write('+(%s)' % (ring_buffer[0][0].timestamp,))
-                # We want to yield all the audio we see from now until
-                # we are NOTTRIGGERED, but we have to start with the
-                # audio that's already in the ring buffer.
-                for f, s in ring_buffer:
-                    voiced_frames.append(f)
-                ring_buffer.clear()
-        else:
-            # We're in the TRIGGERED state, so collect the audio data
-            # and add it to the ring buffer.
-            voiced_frames.append(frame)
-            ring_buffer.append((frame, is_speech))
-            num_unvoiced = len([f for f, speech in ring_buffer if not speech])
-            # If more than 90% of the frames in the ring buffer are
-            # unvoiced, then enter NOTTRIGGERED and yield whatever
-            # audio we've collected.
-            if num_unvoiced > 0.9 * ring_buffer.maxlen:
-                sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
-                triggered = False
-                yield VoicedSnippet(voiced_frames)
-                ring_buffer.clear()
-                voiced_frames = []
-
-    if triggered:
-        sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
-
-    sys.stdout.write('\n')
-    # If we have any leftover voiced audio when we run out of input,
-    # yield it.
-    if voiced_frames:
-        yield VoicedSnippet(voiced_frames)
-
-
 def vad_collector(sample_rate, vad, frames):
     num_padding_frames = 5
     # We use a deque for our sliding window/ring buffer.
     ring_buffer = collections.deque(maxlen=num_padding_frames)
-    # We have two states: TRIGGERED and NOTTRIGGERED. We start in the
-    # NOTTRIGGERED state.
+    # We have two states: TRIGGERED and NOTTRIGGERED. We start in the NOTTRIGGERED state.
     triggered = False
 
     voiced_frames = []
@@ -216,6 +142,7 @@ def vad_collector(sample_rate, vad, frames):
                 # audio that's already in the ring buffer.
                 for f, s in ring_buffer:
                     voiced_frames.append(f)
+
                 ring_buffer.clear()
         else:
             # We're in the TRIGGERED state, so collect the audio data
@@ -237,21 +164,13 @@ def vad_collector(sample_rate, vad, frames):
         sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
 
     sys.stdout.write('\n')
-    # If we have any leftover voiced audio when we run out of input,
-    # yield it.
+    # If we have any leftover voiced audio when we run out of input, yield it.
     if voiced_frames:
         yield VoicedSnippet(voiced_frames)
 
 
-def split_mp3(mp3_file_path, destination_path, df=pd.DataFrame(), translator=None):
-    sample_rate = 16_000
-    samples, sample_length = load_mp3_as_sr16000(mp3_file_path)
-    audio = convert_numpy_samples_to_audio_bytes(samples)
+def join_frame_list(mp3_file_path, destination_path, sample_rate, frames, df=pd.DataFrame(), translator=None):
     vad = webrtcvad.Vad(1)
-    frame_duration_ms = 30
-    frames = frame_generator(frame_duration_ms, audio, sample_rate)
-    frames = list(frames)
-    # snippets = vad_collector_orig(sample_rate, frame_duration_ms, 300, vad, frames)
     snippets = vad_collector(sample_rate, vad, frames)
 
     orig_mp3_file_name = Path(mp3_file_path).name
@@ -275,6 +194,15 @@ def split_mp3(mp3_file_path, destination_path, df=pd.DataFrame(), translator=Non
         result = result.append(snippet_df, ignore_index=True)
 
     return result
+
+
+def split_mp3(mp3_file_path, destination_path, df=pd.DataFrame(), translator=None):
+    samples, _ = load_mp3_as_sr16000(mp3_file_path)
+    audio_bytes = convert_numpy_samples_to_audio_bytes(samples)
+    sample_rate = 16_000
+    frame_duration_ms = 30
+    frames = frame_generator(frame_duration_ms, audio_bytes, sample_rate)
+    return join_frame_list(mp3_file_path, destination_path, sample_rate, list(frames), df, translator)
 
 
 def split_mp3s(ds_id, translator, source_dir, destination_dir):
